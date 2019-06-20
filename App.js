fffede07b6d550f32ee1b3d9838dbcdc950e9b08
Jakub Schulz <jakub104@Dell-Komputer.localdomain>
2019-06-20 21:14:02 +0200

    1. Learn the Basits

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class HelloWorldApp extends Component {
+  render() {
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+          <Text>Hello, world!</Text>
+        </View>
+      );
+  }
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default HelloWorldApp
+
